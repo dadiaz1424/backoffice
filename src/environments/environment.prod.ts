3dfc5a2f174cbd7d08f3a5d013c0a9ebf21b9135
David Diaz <david3096_diaz@hotmail.com>
2023-08-31 12:37:14 -0500

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
     apiResrURL: 'https://arquitectura-1sa89r3l.uc.gateway.dev',
-    apiURLClient: 'https://arquitectura-1sa89r3l.uc.gateway.dev/api',
+    apiURLClient: '',
     apiURLAccounts: 'https://arquitectura-1sa89r3l.uc.gateway.dev',
     // administrationAPIUrl:
     //     'https://banquito-ws-administration-production-6806.up.railway.app',
