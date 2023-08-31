000834529565c7fab434d820e208f7a4c4a8ce4f
David Diaz <david3096_diaz@hotmail.com>
2023-08-31 13:20:32 -0500

    OKI

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
-    apiResrURL: 'dsddsdsd',
-    apiURLClient: '',
+    apiResrURL: 'dsddsdDSDsd',
+    apiURLClient: 'DSDSD',
     apiURLAccounts: 'https://arquitectura-1sa89r3l.uc.gateway.dev',
     // administrationAPIUrl:
     //     'https://banquito-ws-administration-production-6806.up.railway.app',
