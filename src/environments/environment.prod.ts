e720d77dd482fac0d5f7a636a9337bbcac2ab1c2
David Diaz <david3096_diaz@hotmail.com>
2023-08-31 12:45:22 -0500

    Update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiResrURL: 'dsd',
+    apiResrURL: 'dsddsds',
     apiURLClient: '',
     apiURLAccounts: 'https://arquitectura-1sa89r3l.uc.gateway.dev',
     // administrationAPIUrl:
